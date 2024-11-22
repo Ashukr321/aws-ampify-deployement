1ffacca0db19447954ec2e45d62924c8852123b9
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-11-22 10:11:17 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen flex bg-black justify-center items-center">
+
+     <h1 className="text-white text-3xl">welcome to deploying on aws amplify</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
