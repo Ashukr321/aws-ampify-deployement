327534314dac193d2a763990b37218a753693e09
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-11-22 10:22:56 +0530

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ function App() {
 
   return (
     <>
-    <div className="h-screen flex bg-black justify-center items-center">
+    <div className="h-screen flex flex-col gap-4 bg-black justify-center items-center">
 
-     <h1 className="text-white text-3xl">welcome to deploying on aws amplify</h1>
+     <h1 className="text-white text-3xl ">welcome to deploying on aws amplify</h1>
+      <button className="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-10 rounded block">great Ashu</button>
+
     </div>
     </>
   )
